d363f79b59eb383f196c478c7bd06d8a18ee9f0e
pdhung197 <pdhung.info@gmail.com>
2019-04-03 09:20:37 +0700

    Change index newdev

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var hoisting_example = "test_hoisting_AAA";
 document.write("Index javascript");
 (function ($) {
   $("#btn").click(function () {
-    alert("ok men!");
+    alert("OK men!");
   });
   $(document).ready(function () {
     $("#imgid")
